3e9f9c53569bb7b02cb00d9bc04177f481708fe2
Guler Cam <glrsakim@gmail.com>
2024-10-26 14:42:42 +0300

    komut

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Gezegenler Güneşe Uzaklık Sırasına Göre Aşağıda Listelenmiştir :");
+        //foreach döngüsü ile ekrana gezengelerin bilgilerini yazdıralım.
+        for (enumGezegenler gezegenler : enumGezegenler.values()) {
+            System.out.println("Gezegen Adı : " + gezegenler.name());
+            System.out.println("Güneşe yakınlık sırası : " + gezegenler.getSira());
+            System.out.println("Güneşe uzaklığı : " + gezegenler.getUzaklik() + "000 KM");
+            System.out.println("Yarıçapı : " + gezegenler.getYaricap() +" KM");
+            System.out.println("Kendi Etrafında Dönme Süresi : " + gezegenler.getDonmeSuresi() +" Dünya Günü");
+            System.out.println("   ");
     }
 }
--- src/enumGezegenler.java
+++ src/enumGezegenler.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+public enum enumGezegenler {
+    private final int sira; //güneşe uzaklık sırası
+    private final int uzaklik;  // km cinsinden Güneşe uzaklık
+    private final int yaricap;  // km cinsinden yarıçap
+    private final double donmeSuresi;  // dünya günü cinsinden kendi etrafında dönme süresi
 
-public class enumGezegenler {
+    // Constructor
+    enumGezegenler(int sira, int uzaklik, int yaricap, double donmeSuresi) {
+        this.sira = sira;
+        this.uzaklik = uzaklik;
+        this.yaricap = yaricap;
+        this.donmeSuresi = donmeSuresi;
+    }
+
+    // Gezegenleri tanımlayalım
+    MERKUR(1, 57_910, 2_439, 58.65),
+    VENUS(2, 108_200, 6_052, 243),
+    DUNYA(3, 149_600, 6_371, 1),
+    MARS(4, 227_940, 3_390, 1.03),
+    JUPITER(5, 778_330, 69_911, 0.41),
+    SATURN(6, 1_429_400, 58_232, 0.45),
+    URANUS(7, 2_870_990, 25_362, 0.72),
+    NEPTUN(8, 4_504_000, 24_622, 0.67);
+
+    public int getSira(){
+        return sira;
+    }
+
+    public int getUzaklik(){
+        return uzaklik;
+    }
+
+    public int getYaricap(){
+        return yaricap;
+    }
+
+    public double getDonmeSuresi() {
+        return donmeSuresi;
+    }
 }
